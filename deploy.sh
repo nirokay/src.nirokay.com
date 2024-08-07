d29cf67a2c5c9e0b8b1ed4bc350a78701c717378
nirokay <nirokay-public@protonmail.com>
2024-08-07 16:52:40 +0200

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@
 
 source env.sh
 
-./compile-typescript.sh
+./compile-typescript.sh || exit 10
 # cp -r src/javascript/* "$OUTPUT_LN"/javascript || exit 1
 
-cd "$OUTPUT_LN" || exit 2
+cd "$OUTPUT_LN" || exit 20
 git add .
-git commit -m "Automated deployment @ $(date)"
+git commit -m "Automated deployment @ $(date)" || exit 30
 git push
